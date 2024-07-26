46c5ae0df3bb11ae638d899c8c9b885219e3ec20
Anthony Thanpoovong <thanpoovong84@gmail.com>
2024-07-25 20:10:40 -0400

    updated fun facts about me

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -15,39 +15,35 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              CONTACT <span className="purple"> ME </span>
+              Fun Facts About <span className="purple"> Me</span>
             </h1>
             <p className="home-about-body">
-              My name is Anthony Thanpoovong and I'm 5th year Software Engineer
+              My name is Anthony Thanpoovong and I'm 5th year Software Engineering Student
               <br />
-              <br />I am fluent in classics like
+              <br />I'm from South East and Eastern Asian, but I've lived in 
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Toronto, Ontario </b>
               </i>
+              my entire life
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My birthday is on
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
+                <b className="purple"> April Fools </b>
               </i>
+              and coincidentally is the same birthday as Logan Paul and Randy Orton
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              I love travelling and trying new food places. Places I've been to include
+               <b className="purple"> Hong Kong, Thailand, Japan </b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  Miami.
+                  {" "}
                 </b>
               </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              For my graduation trip, I'm planning to go to Las Vegas!
             </p>
           </Col>
           <Col md={4} className="myAvtar">
