e4ebf9296df728b179e02d537d085a78da94f491
Anthony Thanpoovong <thanpoovong84@gmail.com>
2024-08-02 00:17:49 -0400

    added logos for experiences

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ExperienceCard from "./ExperienceCard";
 import Particle from "../Particle";
-import tictactoe from "../../Assets/Projects/tic-tac-toe.png";
+import ezML from "../../Assets/Projects/ezML_2.png";
 import scotiabank from "../../Assets/Projects/scotiabank.png";
-import administration from "../../Assets/Projects/administration-system.png";
-import analysis from "../../Assets/Projects/prison-break.png";
-import doorlock from "../../Assets/Projects/keypad-door-lock.jpg";
+import td from "../../Assets/Projects/td.png";
+import ldrs from "../../Assets/Projects/ldrs.png";
+import bgctk from "../../Assets/Projects/bgctk.png";
+import eastside from "../../Assets/Projects/eastside.png";
 
 function Experience() {
   return (
@@ -21,6 +22,15 @@ function Experience() {
         </p>
         <Row className="justify-content-center mt-4"> {/* Added mt-4 for margin-top */}
           <Col md={4} className="d-flex flex-column align-items-center">
+          <div className="mb-4">
+              <ExperienceCard
+                imgPath={ezML}
+                isBlog={false}
+                title="Helicopter Prison Break Analysis"
+                description="Using Python and Jupyter Notebook, I analyzed all the Helicopter Prison breaks and viewed the statistics using infographics and charts."
+                ghLink="https://github.com/anthonythanpoovong/Helicopter-Prison-Break-Analysis"
+              />
+            </div>
             <div className="mb-4">
               <ExperienceCard
                 imgPath={scotiabank}
@@ -32,7 +42,7 @@ function Experience() {
             </div>
             <div className="mb-4">
               <ExperienceCard
-                imgPath={administration}
+                imgPath={td}
                 isBlog={false}
                 title="Better RAMSS"
                 description="A university student administration system that lets students see their personal information, financial fees, courses, and grades. The admins can see the students in a course, the professors teaching the course and the grades of the students. This application uses Oracle Apex and SQL to manipulate the data and queries given."
@@ -42,7 +52,7 @@ function Experience() {
             </div>
             <div className="mb-4">
               <ExperienceCard
-                imgPath={tictactoe}
+                imgPath={ldrs}
                 isBlog={false}
                 title="Tic-Tac-Toe"
                 description="A Tic-Tac-Toe application that uses Python to let either 1 or 2 players play a game of Tic-Tac-Toe."
@@ -51,16 +61,16 @@ function Experience() {
             </div>
             <div className="mb-4">
               <ExperienceCard
-                imgPath={analysis}
+                imgPath={bgctk}
                 isBlog={false}
-                title="Helicopter Prison Break Analysis"
-                description="Using Python and Jupyter Notebook, I analyzed all the Helicopter Prison breaks and viewed the statistics using infographics and charts."
-                ghLink="https://github.com/anthonythanpoovong/Helicopter-Prison-Break-Analysis"
+                title="Key Pad Door Lock"
+                description="Using C++ and an Arduino Kit, I was able to reuse cardboard as a safe box, to keep any items inside the safe. This project used an Arduino Uno, LCD, 4x4 Keypad, Linear Solenoid Lock, and IRF510N MOSFET."
+                ghLink="https://github.com/anthonythanpoovong/Keypad-Door-Lock"
               />
             </div>
             <div className="mb-4">
               <ExperienceCard
-                imgPath={doorlock}
+                imgPath={eastside}
                 isBlog={false}
                 title="Key Pad Door Lock"
                 description="Using C++ and an Arduino Kit, I was able to reuse cardboard as a safe box, to keep any items inside the safe. This project used an Arduino Uno, LCD, 4x4 Keypad, Linear Solenoid Lock, and IRF510N MOSFET."
