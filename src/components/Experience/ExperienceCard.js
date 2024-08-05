2baa578534ff59901aac3993aa48bc42419b382a
Anthony Thanpoovong <thanpoovong84@gmail.com>
2024-08-05 19:53:43 -0400

    removing unused imports

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
 import { Row, Col } from "react-bootstrap";
 import "./ExperienceCard.css"; // Import the CSS file
 
