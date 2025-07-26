6aabdec29264f2861fe237841d81675dd9d54c62
Anthony Thanpoovong <thanpoovong84@gmail.com>
2025-07-26 02:46:04 -0400

    changed type writer message

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,16 +1,10 @@
-import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
     <Typewriter
       options={{
-        strings: [
-          "Software Engineer",
-          "Software Developer",
-          "5th Year Software Engineering Student",
-          "Looking for New Graduate Opportunities",
-        ],
+        strings: ["Software Engineer", "Security Engineer", "AI Enthusiast"],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
